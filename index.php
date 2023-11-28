43ec67a92d8d7a6f666fd4d1dbacf0f24387d11f
DavideGila <davide.gila@gmail.com>
2023-11-28 14:33:37 +0100

    scaffholding + first snak array

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+$matches = [
+    [
+        'teamHome' => 'Olimpia Milano',
+        'teamAway' => 'Cantù',
+        'scoreHome' => '55',
+        'scoreAway' => '60',        
+    ],
+    [
+        'teamHome' => 'Los Angeles Lakers',
+        'teamAway' => 'Chicago Bulls',
+        'scoreHome' => '70',
+        'scoreAway' => '68',        
+    ],
+    [
+        'teamHome' => 'Boston Celtics',
+        'teamAway' => 'Phoenix Suns',
+        'scoreHome' => '80',
+        'scoreAway' => '35',        
+    ],
+    [
+        'teamHome' => 'Miami Heat',
+        'teamAway' => 'Golden State Warriors',
+        'scoreHome' => '58',
+        'scoreAway' => '46',        
+    ]
+]
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
